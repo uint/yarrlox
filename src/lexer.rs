343679190514854e169874bc8e3019303c48569e
Tomasz Kurcz <uint@lavabit.com>
2022-10-07 17:23:23 +0200

    lexer: skip comments

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use logos::Logos;
+use logos::{Filter, Lexer, Logos};
 
 #[derive(Clone, Debug, PartialEq, Eq, Logos)]
 pub enum Token<'source> {
@@ -86,38 +86,63 @@ pub enum Token<'source> {
     #[token("while")]
     While,
 
-    // Invalid tokens
+    // Errors, logic for skipping comments and whitespace
     #[error]
-    #[regex(r"[ \t\f]+", logos::skip)] // ignore whitespace
-    Error,
+    #[regex(r#"//[^\n]*"#, logos::skip)] // ignore single line comments
+    #[regex(r"[ \n\t\f]+", logos::skip)] // ignore whitespace
+    InvalidToken,
+
+    #[token("/*", skip_block_comment)]
+    UnclosedBlockComment,
+}
+
+fn skip_block_comment<'source>(lex: &mut Lexer<'source, Token<'source>>) -> Filter<()> {
+    if let Some(ix) = lex.remainder().find("*/") {
+        lex.bump(ix + 2);
+        Filter::Skip
+    } else {
+        // emit UnclosedBlockComment
+        Filter::Emit(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::VecDeque;
-
     use super::*;
 
+    trait IntoTokens {
+        fn into_tokens(self) -> Vec<Token<'static>>;
+    }
+
+    impl IntoTokens for &'static str {
+        fn into_tokens(self) -> Vec<Token<'static>> {
+            Token::lexer(self).into_iter().collect()
+        }
+    }
+
+    impl<const N: usize> IntoTokens for [Token<'static>; N] {
+        fn into_tokens(self) -> Vec<Token<'static>> {
+            self.to_vec()
+        }
+    }
+
     #[track_caller]
-    fn assert_lexer<'s>(src: &'s str, expected: impl IntoIterator<Item = Token<'s>>) {
-        let mut lexer = Token::lexer(src);
+    fn assert_lexer(left: impl IntoTokens, right: impl IntoTokens) {
+        let left_vec = left.into_tokens();
+        let right_vec = right.into_tokens();
 
-        let expected: Vec<_> = expected.into_iter().collect();
-
-        for expected in expected {
-            let token = lexer.next().unwrap();
-            if token != expected {
+        for (left, right) in left_vec.clone().into_iter().zip(right_vec.clone()) {
+            if left != right {
                 panic!(
-                    "Unexpected token. Expected: {:?}, got: {}",
-                    expected,
-                    lexer.slice()
+                    "tokens didn't match: left: {:?}, right: {:?}\nleft: {:?}\nright: {:?}",
+                    left, right, left_vec, right_vec
                 );
             }
         }
     }
 
     #[test]
-    fn tokenize_strings() {
+    fn string_literals() {
         use Token::*;
 
         assert_lexer(
@@ -151,5 +176,35 @@ mod tests {
                 Semicolon,
             ],
         );
+    }
+
+    #[test]
+    fn whitespace() {
+        use Token::*;
+
+        assert_lexer("var var", [Var, Var]);
+        assert_lexer("var var", "var    var");
+        assert_lexer("var var", "\n\t\t\nvar  \n\n\t  var ");
+    }
+
+    #[test]
+    fn ignore_line_comments() {
+        use Token::*;
+
+        assert_lexer("var var // () \n var", [Var, Var, Var]);
+    }
+
+    #[test]
+    fn ignore_block_comments() {
+        use Token::*;
+
+        assert_lexer("var var /* var a * / = 5; */ ;", [Var, Var, Semicolon]);
+    }
+
+    #[test]
+    fn unclosed_block_comment() {
+        use Token::*;
+
+        assert_lexer("var var /* var a = 5;", [Var, Var, UnclosedBlockComment]);
     }
 }
