f905083c286690190beb85fb01fb29d33ea48c1c
Tomasz Kurcz <uint@lavabit.com>
2022-10-31 13:22:23 +0100

    Fix function env

--- src/env.rs
+++ src/env.rs
@@ -3,47 +3,68 @@ use std::collections::{HashMap, VecDeque};
 use crate::value::Value;
 
 pub struct Env {
-    /// This is used as a stack. The front of the stack is the current (innermost, leaf) env.
+    globals: HashMap<String, Value>,
+    /// This is used as a series of stacks. The front of the stack is the current
+    /// (innermost, leaf) env.
     /// The reason to diverge from the book is to avoid trouble with mutable references
     /// and having Envs be a recursively defined type.
-    scopes: VecDeque<HashMap<String, Value>>,
+    branches: VecDeque<VecDeque<HashMap<String, Value>>>,
 }
 
 impl Env {
     pub fn new() -> Self {
         Self {
-            scopes: [HashMap::new()].into(),
+            globals: HashMap::new(),
+            branches: [[].into()].into(),
         }
     }
 
+    pub fn branch(&mut self) {
+        self.branches.push_front([HashMap::new()].into());
+    }
+
+    pub fn pop_branch(&mut self) {
+        self.branches.pop_front();
+    }
+
     pub fn child(&mut self) {
-        self.scopes.push_front(HashMap::new());
+        self.branches[0].push_front(HashMap::new());
     }
 
     pub fn pop(&mut self) {
-        #[cfg(debug_assertions)]
-        assert!(
-            self.scopes.len() >= 2,
-            "attempting to remove root environment"
-        );
-        self.scopes.pop_front();
+        self.branches[0].pop_front().expect("nothing to pop");
     }
 
     fn global(&mut self) -> &mut HashMap<String, Value> {
-        let last = self.scopes.len() - 1;
-        &mut self.scopes[last]
+        &mut self.globals
     }
 
     fn current(&mut self) -> &mut HashMap<String, Value> {
-        &mut self.scopes[0]
+        if let Some(scope) = self.branches[0].get_mut(0) {
+            scope
+        } else {
+            &mut self.globals
+        }
     }
 
     pub fn define(&mut self, name: impl Into<String>, value: Value) {
         self.current().insert(name.into(), value);
     }
 
+    fn walk_env(&self) -> impl Iterator<Item = &HashMap<String, Value>> {
+        self.branches[0]
+            .iter()
+            .chain(std::iter::once(&self.globals))
+    }
+
+    fn walk_env_mut(&mut self) -> impl Iterator<Item = &mut HashMap<String, Value>> {
+        self.branches[0]
+            .iter_mut()
+            .chain(std::iter::once(&mut self.globals))
+    }
+
     pub fn get(&self, name: &str) -> Value {
-        for scope in self.scopes.iter() {
+        for scope in self.walk_env() {
             if let Some(v) = scope.get(name) {
                 return v.clone();
             }
@@ -53,7 +74,7 @@ impl Env {
     }
 
     pub fn assign(&mut self, name: String, value: Value) -> Result<(), EnvError> {
-        for scope in self.scopes.iter_mut() {
+        for scope in self.walk_env_mut() {
             #[allow(clippy::map_entry)]
             if scope.contains_key(&name) {
                 scope.insert(name, value);
