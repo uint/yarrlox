3f09f7c11b74f65358eca2c76483bcc5835dfe13
Tomasz Kurcz <uint@lavabit.com>
2022-11-04 19:02:14 +0100

    Add some tests!

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,6 +4,134 @@ use runner::run;
 
 use yarrlox::value::Value;
 
+// These aren't meant to be complete branch coverage tests or anything like that. These
+// are smoke tests for key functionality. There's the official Lox test suite already.
+// Once the interpreter is finished, I'll run it against that.
+
+#[test]
+fn var_definition() {
+    let closure = r#"
+var x = 5;
+return x;
+"#;
+
+    run(closure).assert_v(Value::Num(5.));
+}
+
+#[test]
+fn undefined_var_is_nil() {
+    let closure = r#"
+return x;
+"#;
+
+    run(closure).assert_v(Value::Nil);
+}
+
+#[test]
+fn var_out_of_scope() {
+    let closure = r#"
+{
+  var x = 5;
+}
+return x;
+"#;
+
+    run(closure).assert_v(Value::Nil);
+}
+
+#[test]
+fn shadowing() {
+    let closure = r#"
+var x = 5;
+
+{
+  var x = 10;
+  print x;
+}
+
+return x;
+"#;
+
+    let run = run(closure);
+    run.assert_output("10");
+    run.assert_v(Value::Num(5.));
+}
+
+#[test]
+fn mutation_in_a_scope() {
+    let closure = r#"
+var x = 5;
+
+{
+  x = 10;
+  print x;
+}
+
+return x;
+"#;
+
+    let run = run(closure);
+    run.assert_output("10");
+    run.assert_v(Value::Num(10.));
+}
+
+#[test]
+fn string_concat() {
+    let closure = r#"
+return "foo" + "bar";
+"#;
+
+    run(closure).assert_v(Value::string("foobar"));
+}
+
+#[test]
+fn fun_def() {
+    let closure = r#"
+fun pow(x) {
+  return x * x;
+}
+
+return pow(5);
+"#;
+
+    run(closure).assert_v(Value::Num(25.));
+}
+
+#[test]
+fn partial_application() {
+    let closure = r#"
+fun adder(x) {
+  fun result(a) {
+    return x + a;
+  }
+
+  return result;
+}
+
+var my_adder = adder(3);
+return my_adder(5);
+"#;
+
+    run(closure).assert_v(Value::Num(8.));
+}
+
+#[test]
+fn fun_with_fun_as_arg() {
+    let closure = r#"
+fun inc(x) {
+  return x + 1;
+}
+
+fun applier(fn, el) {
+  return fn(el);
+}
+
+return applier(inc, 5);
+"#;
+
+    run(closure).assert_v(Value::Num(6.));
+}
+
 #[test]
 fn closure() {
     let closure = r#"
